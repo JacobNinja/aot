6469c3ae4f8602ce302e5e191fe9faffd9e02476
Jacob Richardson <jacob@rafflecopter.com>
2015-08-04 10:24:26 -0600

    Update project and version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject aot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject aot "0.1.0"
+  :description "AOT compile dependencies found in namespace declarations"
+  :url "http://github.com/JacobNinja/aot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
